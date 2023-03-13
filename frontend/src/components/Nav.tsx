99f1f073d78eed53254f4fbd6297016fc75480be
Striller18 <oscaradell16@gmail.com>
2023-03-13 21:51:52 +0100

    finish nav, add host tag to package.json dev, add colors tailwind

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -4,20 +4,28 @@ import { BiScan} from "react-icons/bi";
 type navData = {
     actualPage: string;
     lastPage: string;
+    changePage: (newPage: string) => void;
 }
 
 const iconActive = {
-    "color": "#75BA75",
-    "border-color": "#548654"
+    "color": '#75BA75',
+    "borderColor": '#548654',
+    "transition": 'all ease-in 0.2s'
+    // "fontSize": '2.6rem',
+}
+
+const navContainerStyle = {
+    boxShadow: "0px -5px 10px rgba(25, 25, 25, 0.25)",
 }
 
 function Nav(props: navData){
-    return <div className='flex justify-evenly px-10 py-2.5 fixed bottom-0 w-full text-4xl'>
-        <div><FaListAlt style={ props.actualPage === "lists" ? iconActive : {}}/></div>
-        <div><BiScan    style={ props.actualPage === "scan" ? iconActive : {}}/></div>
-        <div><FaUserAlt style={ props.actualPage === "user" ? iconActive : {}}/></div>
-    </div>
-    
+    return (
+        <div className='flex justify-between items-end px-14 py-4 fixed bottom-0 h-[4rem] w-full text-3xl' style = {navContainerStyle}>
+            <div onClick={() => props.changePage("scan")}><BiScan       style={ props.actualPage === "scan" ? iconActive : {}}/></div>
+            <div onClick={() => props.changePage("lists")}><FaListAlt   style={ props.actualPage === "lists" ? iconActive : {}}/></div>
+            <div onClick={() => props.changePage("user")}><FaUserAlt    style={ props.actualPage === "user" ? iconActive : {}}/></div>
+        </div>
+    )
 }
 
 export default Nav;
--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -4,6 +4,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      green: '#75BA75',
+      white: '#eeeeee',
+      grey: '#949494',
+      darkGrey: "#616161",
+      black: '#363636',
+    },
     extend: {},
   },
   plugins: [],
