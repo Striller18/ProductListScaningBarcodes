14e8fdf9987b571b9f995f5c40e6230b61c1d60c
Striller18 <oscaradell16@gmail.com>
2023-03-13 16:53:56 +0100

    react icons installed and first step nav done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,24 @@
-import { useState } from 'react'
+import { MouseEventHandler, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import Nav  from "./components/Nav";
-import './App.css'
+import './css/App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-  const [actualPage, setActualPage]= useState("home")
+  const [actualPage, setActualPage]= useState("lists")
   const [lastPage, setLastPage]= useState("")
+
+  const btnClick = (newPage: string)=> {
+    setLastPage(actualPage); 
+    setActualPage(newPage)
+  }
 
   return (
     <div className="App">
-      <h1 className='font-bold'>Vite + React + Typescript + Tailwind CSS 3</h1>
-      <div className="card">
-        {actualPage==="home" ? 
-        <button onClick={() => {
-          setLastPage(actualPage); 
-          setActualPage("prueba")}
-        }>go prueba</button>
-         :
-          <h1>reload :)</h1>
-        }
+      <div>
+        <button onClick={() => btnClick("lists")}>lists</button>
+        <button onClick={() => btnClick("scan")}>scan</button>
+        <button onClick={() => btnClick("user")}>user</button>
       </div>
       <Nav actualPage={actualPage} lastPage={lastPage}></Nav>
     </div>
--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -1,11 +1,22 @@
+import { FaUserAlt,FaListAlt } from 'react-icons/fa';
+import { BiScan} from "react-icons/bi";
 
 type navData = {
     actualPage: string;
     lastPage: string;
 }
 
+const iconActive = {
+    "color": "#75BA75",
+    "border-color": "#548654"
+}
+
 function Nav(props: navData){
-    return <h1>Esta en la pagina {props.actualPage}</h1>
+    return <div className='flex justify-evenly px-10 py-2.5 fixed bottom-0 w-full text-4xl'>
+        <div><FaListAlt style={ props.actualPage === "lists" ? iconActive : {}}/></div>
+        <div><BiScan    style={ props.actualPage === "scan" ? iconActive : {}}/></div>
+        <div><FaUserAlt style={ props.actualPage === "user" ? iconActive : {}}/></div>
+    </div>
     
 }
 
