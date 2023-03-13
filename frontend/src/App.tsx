919387d8a3a886d9d4405a5e2448b61fe32c32b8
Striller18 <oscaradell16@gmail.com>
2023-03-13 21:55:53 +0100

    create layout app, page navigation, hook actualPage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,34 @@
-import { MouseEventHandler, useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { MouseEventHandler, useState, ReactElement} from 'react';
 import Nav  from "./components/Nav";
-import './css/App.css'
+import './css/App.css';
+import Lists from './pages/Lists';
 
 function App() {
   const [count, setCount] = useState(0)
-  const [actualPage, setActualPage]= useState("lists")
-  const [lastPage, setLastPage]= useState("")
+  const [actualPage, setActualPage] = useState("lists")
+  const [lastPage, setLastPage] = useState("")
 
-  const btnClick = (newPage: string)=> {
+  const lists = [
+    {id:0, name: "prueba1", products: [{}], members: [{}]},
+    {id:0, name: "prueba2", products: [{}], members: [{}]},
+    {id:0, name: "prueba3", products: [{}], members: [{}]},
+  ];
+
+  const pages: Record<string, ReactElement>= {
+    lists: <Lists lists={lists}/>,
+    user: <h1>User</h1>,
+    scan: <h1>Scan</h1>,
+  }
+
+  const handlerChangeNav = (newPage: string) => {
     setLastPage(actualPage); 
     setActualPage(newPage)
   }
 
   return (
-    <div className="App">
-      <div>
-        <button onClick={() => btnClick("lists")}>lists</button>
-        <button onClick={() => btnClick("scan")}>scan</button>
-        <button onClick={() => btnClick("user")}>user</button>
-      </div>
-      <Nav actualPage={actualPage} lastPage={lastPage}></Nav>
+    <div className="flex justify-center">
+      {pages[actualPage]}
+      <Nav changePage={handlerChangeNav} actualPage={actualPage} lastPage={lastPage} ></Nav>
     </div>
   )
 }
