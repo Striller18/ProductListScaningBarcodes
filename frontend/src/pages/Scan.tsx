48e441fcf9cb85f3da5ab2ade0e02d33679fd691
Striller18 <oscaradell16@gmail.com>
2023-03-14 16:54:52 +0100

    style and optimize Scan, start use quagga

--- frontend/src/pages/Scan.tsx
+++ frontend/src/pages/Scan.tsx
@@ -5,7 +5,6 @@ const Scan = () => {
     const [code, setCode] = useState<string>('');
     const [mediaDevices, setMediaDevices] = useState<MediaDeviceInfo[]>([]);
     const [actualDeviceId, setActualDeviceId] = useState<string>("");
-
     const videoRef = useRef<HTMLVideoElement>(document.createElement("video"));
 
     navigator.mediaDevices.enumerateDevices().then((data)=> setMediaDevices(data));
@@ -23,26 +22,36 @@ const Scan = () => {
         navigator.mediaDevices.getUserMedia({video: {deviceId: id}})
         .then((stream)=> videoRef.current.srcObject = stream)
         .catch((error) => {
-            if (error.name === "ConstraintNotSatisfiedError") {
-                console.error("Resolution is not supported");
-            } else if (error.name === "PermissionDeniedError") {
-                console.error(
-                "You need to grant this page permission to access your camera and microphone."
-                );
-            } else {
-                console.error(`getUserMedia error: ${error.name}`, error);
-            }
+            if (error.name === "ConstraintNotSatisfiedError") alert("Resolution is not supported");
+            else if (error.name === "PermissionDeniedError") alert("You need to grant this page permission to access your camera and microphone.");
+            else console.error(`getUserMedia error: ${error.name}`, error)
         });
     }
 
+    // Quagga.init({
+    //     inputStream : {
+    //       name : "Live",
+    //       type : "LiveStream",
+    //       target: document.createElement("div")  // Or '#yourElement' (optional)
+    //     },
+    //     decoder : {
+    //       readers : ["code_128_reader"]
+    //     }
+    // }, function(err) {
+    //       if (err) {
+    //           console.log(err);
+    //           return
+    //       }
+    //       console.log("Initialization finished. Ready to start");
+    //       Quagga.start();
+    // });
+
     return (
-        <div className='flex'>
+        <div className='flex flex-col gap-5 w-full'>
             <select className='rounded-full px-4 h-8 bg-darkGrey' onChange={(event)=> setVideoDevice(event.target.value)}>
-            {
-                mediaDevices.map(getOption)
-            }
+                {mediaDevices.map(getOption)}
             </select>
-            <video ref={videoRef} autoPlay={true}></video>
+            <video className='rounded-md bg-darkGrey' id="cam" ref={videoRef} autoPlay={true}>No source</video>
             <p> Code: {code} </p>
         </div>
     )
