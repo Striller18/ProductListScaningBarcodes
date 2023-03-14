46ed5916d0be181c4e75e4b6de95a81135590638
Striller18 <oscaradell16@gmail.com>
2023-03-14 09:31:45 +0100

    added dbTmp

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,20 +2,16 @@ import { MouseEventHandler, useState, ReactElement} from 'react';
 import Nav  from "./components/Nav";
 import './css/App.css';
 import Lists from './pages/Lists';
+import db from './assets/dbTmp.json';
 
 function App() {
   const [count, setCount] = useState(0)
   const [actualPage, setActualPage] = useState("lists")
   const [lastPage, setLastPage] = useState("")
 
-  const lists = [
-    {id:0, name: "prueba1", products: [{}], members: [{}]},
-    {id:0, name: "prueba2", products: [{}], members: [{}]},
-    {id:0, name: "prueba3", products: [{}], members: [{}]},
-  ];
 
   const pages: Record<string, ReactElement>= {
-    lists: <Lists lists={lists}/>,
+    lists: <Lists lists={db.lists}/>,
     user: <h1>User</h1>,
     scan: <h1>Scan</h1>,
   }
